5ad0581b2a3862d824bb526bdf4c89b78d039caa
Lukas Gruber <lgruber.mmt-b2016@fh-salzburg.ac.at>
2019-01-30 11:31:29 +0100

    feat(api): add kernel exception listener
    Stop exceptions from bubbling to top, return json error response

---
+++ src/AppBundle/EventListener/KernelExceptionListener.php
@@ -0,0 +1,33 @@
+<?php
+namespace AppBundle\EventListener;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+
+class KernelExceptionListener
+{
+    /**
+     * Listen for exceptions and return json error response.
+     *
+     * @param GetResponseForExceptionEvent $event
+     */
+    public function onKernelException(GetResponseForExceptionEvent $event)
+    {
+        $exception = $event->getException();
+
+        $response = new JsonResponse([
+            'error' => $exception->getMessage()
+        ]);
+
+        if ($exception instanceof HttpExceptionInterface) {
+            $response->setStatusCode($exception->getStatusCode());
+            $response->headers->replace($exception->getHeaders());
+        } else {
+            $response->setStatusCode(Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        $event->setResponse($response);
+    }
+}
