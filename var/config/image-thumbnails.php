1951c22cd7f735c60b2cc073f5ea8958969aab04
Lukas Gruber <lgruber.mmt-b2016@fh-salzburg.ac.at>
2019-04-07 01:08:07 +0200

    feat: add thumbnails config file

---
+++ var/config/image-thumbnails.php
@@ -0,0 +1,130 @@
+<?php 
+
+return [
+    "eventCategoryOverview" => [
+        "items" => [
+            [
+                "method" => "cover",
+                "arguments" => [
+                    "width" => 500,
+                    "height" => 500,
+                    "positioning" => "center",
+                    "forceResize" => FALSE
+                ]
+            ]
+        ],
+        "medias" => [
+
+        ],
+        "name" => "eventCategoryOverview",
+        "description" => "",
+        "group" => "",
+        "format" => "SOURCE",
+        "quality" => 85,
+        "highResolution" => 0.0,
+        "preserveColor" => FALSE,
+        "preserveMetaData" => FALSE,
+        "rasterizeSVG" => FALSE,
+        "downloadable" => FALSE,
+        "modificationDate" => 1548842786,
+        "creationDate" => 1548842766,
+        "id" => "eventCategoryOverview"
+    ],
+    "heroSlide" => [
+        "items" => [
+            [
+                "method" => "cover",
+                "arguments" => [
+                    "width" => 1920,
+                    "height" => 1075,
+                    "positioning" => "center",
+                    "forceResize" => FALSE
+                ]
+            ]
+        ],
+        "medias" => [
+            "575w" => [
+                [
+                    "method" => "cover",
+                    "arguments" => [
+                        "width" => 575,
+                        "height" => 322,
+                        "positioning" => "center",
+                        "forceResize" => FALSE
+                    ]
+                ]
+            ]
+        ],
+        "name" => "heroSlide",
+        "description" => "",
+        "group" => "",
+        "format" => "SOURCE",
+        "quality" => 85,
+        "highResolution" => 0.0,
+        "preserveColor" => FALSE,
+        "preserveMetaData" => FALSE,
+        "rasterizeSVG" => FALSE,
+        "downloadable" => FALSE,
+        "modificationDate" => 1552233998,
+        "creationDate" => 1552065675,
+        "id" => "heroSlide"
+    ],
+    "imageRadio" => [
+        "items" => [
+            [
+                "method" => "cover",
+                "arguments" => [
+                    "width" => 350,
+                    "height" => 196,
+                    "positioning" => "center",
+                    "forceResize" => FALSE
+                ]
+            ]
+        ],
+        "medias" => [
+
+        ],
+        "name" => "imageRadio",
+        "description" => "",
+        "group" => "",
+        "format" => "SOURCE",
+        "quality" => 85,
+        "highResolution" => 0.0,
+        "preserveColor" => FALSE,
+        "preserveMetaData" => FALSE,
+        "rasterizeSVG" => FALSE,
+        "downloadable" => FALSE,
+        "modificationDate" => 1552400120,
+        "creationDate" => 1552400107,
+        "id" => "imageRadio"
+    ],
+    "lazy--heroSlide" => [
+        "items" => [
+            [
+                "method" => "cover",
+                "arguments" => [
+                    "width" => 32,
+                    "height" => 18,
+                    "positioning" => "center",
+                    "forceResize" => FALSE
+                ]
+            ]
+        ],
+        "medias" => [
+
+        ],
+        "name" => "lazy--heroSlide",
+        "description" => "",
+        "group" => "",
+        "format" => "SOURCE",
+        "quality" => 30,
+        "highResolution" => 0.0,
+        "preserveColor" => FALSE,
+        "preserveMetaData" => FALSE,
+        "rasterizeSVG" => FALSE,
+        "downloadable" => FALSE,
+        "modificationDate" => 1553769833,
+        "creationDate" => 1553769487,
+        "id" => "lazy--heroSlide"
+    ]
+];
