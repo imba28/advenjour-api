0c1f98f8ecbde147718efec301d9ab378646d849
Lukas Gruber <lgruber.mmt-b2016@fh-salzburg.ac.at>
2019-04-25 13:19:55 +0200

    fix: import namespace

--- src/AppBundle/JsonAPI/ErrorObject.php
+++ src/AppBundle/JsonAPI/ErrorObject.php
@@ -1,6 +1,7 @@
 <?php
 namespace AppBundle\JsonAPI;
 
+use Pimcore;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
