961c557f6124f0ed3a276db2578660a0eb192259
Stefan Elsenhuber <selsenhuber.mmt-b2016@fh-salzburg.ac.at>
2019-05-23 10:13:00 +0200

    update EventCategory Serializer add thumbnail

--- src/AppBundle/Serializer/EventCategorySerializer.php
+++ src/AppBundle/Serializer/EventCategorySerializer.php
@@ -35,7 +35,8 @@ class EventCategorySerializer extends AbstractSerializer
         }
 
         $this->getSerializer(Asset::class)->setThumbnails([
-            'eventCategoryOverview'
+            'eventCategoryOverview',
+            'checkBoxImage'
         ]);
 
         $singleResource = $this->getSingleResource($object->getId(), $object->getClassName());
