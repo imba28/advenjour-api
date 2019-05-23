2a3c2ca0b4b8785ebe340243b86a4ebdc814050d
Lukas Gruber <lgruber.mmt-b2016@fh-salzburg.ac.at>
2019-05-23 16:06:52 +0200

    feat: log all unhandled exceptions

--- src/AppBundle/EventListener/KernelExceptionListener.php
+++ src/AppBundle/EventListener/KernelExceptionListener.php
@@ -3,6 +3,8 @@ namespace AppBundle\EventListener;
 
 use AppBundle\JsonAPI\Document;
 use AppBundle\JsonAPI\ErrorObject;
+use Pimcore\Log\ApplicationLogger;
+use Pimcore\Tool;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
@@ -10,6 +12,20 @@ use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 
 class KernelExceptionListener
 {
+    /**
+     * @var ApplicationLogger
+     */
+    private $logger;
+
+    /**
+     * KernelExceptionListener constructor.
+     * @param ApplicationLogger $logger
+     */
+    public function __construct(ApplicationLogger $logger)
+    {
+        $this->logger = $logger;
+    }
+
     /**
      * Listen for exceptions and return json error response.
      *
@@ -28,6 +44,10 @@ class KernelExceptionListener
         } else {
             $response->setStatusCode(Response::HTTP_INTERNAL_SERVER_ERROR);
         }
+
+        $this->logger->error($exception->getMessage() . "\n\n". $exception->getTraceAsString() . "\n\n" . Tool::getClientIp(), [
+            'component' => $exception->getTrace()[0]['class'],
+        ]);
 
         $event->setResponse($response);
     }
